dc3ecd00a7a2b3599a052931f53dad0fdf7a2cfe
Alisan Cabezas <alisanxoc@gmail.com>
2025-03-25 01:27:08 -0400

    javascript

---
+++ assignment6/script.js
@@ -0,0 +1,11 @@
+console.log("CSS + JavaScript is powerful!");
+
+// Show an alert message when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 5 with JavaScript!");
+};
+
+// Function to toggle between light mode and dark mode
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
