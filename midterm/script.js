0505a2dd422b8908b3e4ceffd617c28de755af69
Alisan Cabezas <alisanxoc@gmail.com>
2025-04-08 15:08:25 -0400

    done

--- midterm/script.js
+++ midterm/script.js
@@ -32,3 +32,15 @@ const estimates = [
     resultBox.innerText = "No estimate found for that combination.";
   }
 
+  const colors = ["#3e88ef", "#fefff7", "#f7fff7"]; // blue, yellow, green
+  console.log("The first color in the array is:", colors[0]);
+  
+  const userName = prompt("What is your name?");
+  const colorChoice = prompt("Pick a background color: 0 for blue, 1 for yellow, 2 for green");
+  
+  const userInfo = {
+    name: userName,
+    selectedColor: colors[colorChoice]
+  };
+  
+  document.body.style.backgroundColor = userInfo.selectedColor;
